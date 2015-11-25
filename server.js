9139b708e5c3019e61ee087b7b694ebc1b642511
simoncrabtree <simon@crabsticks.net>
2015-11-25 13:22:51 +0000

    Show git commit id and log it out after cloning

--- server.js
+++ server.js
@@ -11,8 +11,9 @@ server.route({
     console.log("Incoming message from GitHub", request.payload);
     reply('OK');
 
-    git.Clone.clone('https://github.com/limajs/hubble.git', 'work_dir', null, function(repo) {
-      console.log("Cloned");
+    git.Clone.clone('https://github.com/limajs/hubble.git', 'work_dir', function(repo) {
+      
+      console.log("Cloned", request.payload.after);
     });
   }
 });
