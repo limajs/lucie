ca5411ccd46c85476637fb60a41290c703811a6d
simoncrabtree <simon@crabsticks.net>
2015-11-25 13:13:34 +0000

    Clone repo on post

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var Hapi = require('hapi');
+var git = require('nodegit');
 
 var server = new Hapi.Server();
 server.connection({ port: 3000 });
@@ -9,8 +10,12 @@ server.route({
   handler: function(request, reply) {
     console.log("Incoming message from GitHub", request.payload);
     reply('OK');
+
+    git.Clone.clone('https://github.com/limajs/hubble.git', 'work_dir', null, function(repo) {
+      console.log("Cloned");
+    });
   }
-})
+});
 
 server.start(function () {
     console.log('Server running at:', server.info.uri);
